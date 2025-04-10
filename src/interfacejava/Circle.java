64f0f622ccb64afcccdb2a90f598bb8dafab634a
remya <remya240@gmail.com>
2025-04-10 16:15:10 -0700

    Addded code for Rectangle class and interface and Circle class

--- src/interfacejava/Circle.java
+++ src/interfacejava/Circle.java
@@ -1,5 +1,19 @@
 package interfacejava;
 
-public class Circle {
+public class Circle implements Shape1 {
+
+	public void makeshape() {
+		System.out.println("The shape is circle");
+	}
+
+	public static void main(String[] args) {
+		Circle shape = new Circle();
+		shape.makeshape();
+	}
+
+}
+
+interface Shape1 {
+	void makeshape();
 
 }
--- src/interfacejava/Rectangle.java
+++ src/interfacejava/Rectangle.java
@@ -1,5 +1,18 @@
 package interfacejava;
 
-public class Rectangle {
+public class Rectangle implements Shape {
+	public void makeshape() {
+		System.out.println("Shape is Rectangle");
+	}
+
+	public static void main(String[] args) {
+		Rectangle shape = new Rectangle();
+		shape.makeshape();
+	}
+
+}
+
+interface Shape {
+	void makeshape();
 
 }
