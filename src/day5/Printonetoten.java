61a785305553566d50f82df5afe9e752a9ee4c91
remya <remya240@gmail.com>
2025-04-18 19:03:12 -0700

    Day5

---
+++ src/day5/Printonetoten.java
@@ -0,0 +1,16 @@
+package day5;
+
+public class Printonetoten {
+public static void main(String[] args) {
+	for(int i=0;i<=10;i++) {
+		System.out.println(i);
+		
+	}
+	  int i = 10;
+	  while(i >= 1) {
+		System.out.println(i);
+		i--;
+
+	}
+}
+}
